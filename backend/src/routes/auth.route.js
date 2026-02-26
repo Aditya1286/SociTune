4302bac65757c344fbc9265c22e44770922b1f52
Aditya Aishwarya <aishwaryaaditya2@gmail.com>
2026-02-26 23:40:16 +0530

    wireframing backend & frontend

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,6 @@
+import {Router} from "express";
+const router = Router();
+router.get('/',(req,res)=>{
+    res.send("Auth route with GET method");
+})
+export default router;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
+
+const App = () => {
+  return (
+    <header>
+      {/* Show the sign-in and sign-up buttons when the user is signed out */}
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      {/* Show the user button when the user is signed in */}
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
+}
+
+export default App
