585803faeaa5fcea58b99961c49d1cd794e592b6
Aditya Aishwarya <aishwaryaaditya2@gmail.com>
2026-04-30 20:51:40 +0530

    Homepage UI updated

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import { createSong , deleteSong , createAlbum , deleteAlbum} from "../controller/admin.controller.js";
+import { createSong , deleteSong , createAlbum , deleteAlbum,checkAdmin} from "../controller/admin.controller.js";
 import { protectRoute,requireAdmin } from "../middleware/auth.middleware.js";
 const router = Router();
 // router.get('/',getAdmin);
 router.post('/songs',protectRoute,requireAdmin,createSong);
 router.delete('/songs/:id',protectRoute,requireAdmin,deleteSong);
-
+router.get("/check", checkAdmin);
 router.post('/albums',protectRoute,requireAdmin,createAlbum);
 router.delete('/albums/:id',protectRoute,requireAdmin,deleteAlbum);
 
